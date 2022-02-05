da936fc59f72fdfce827bd42a8cd2bdbbcc7646d
Irfan Danish <irfandanishrajput@gmail.com>
2022-02-05 16:06:00 +0500

    Tasks
    Updated the bundle identifier
    Changed app icon
    Updated pods configuration so that everything a build is generated the target is set to 11.0

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'CHEEMS PETS',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.orange,
           fontFamily: 'Montserrat',
